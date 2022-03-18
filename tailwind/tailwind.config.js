fd892d1074d4cd5735ea9ff9ffc47a88a22b5760
John Doe <alirezanjprogrammer110@gmail.com>
2022-03-18 03:40:06 +0330

    edit sidBar and javascript

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["../index.html", "./style/index.css", "../js/pages/Dashbord.js" , "../js/index.js"],
+  content: ["../index.html","../page/home.html", "./style/index.css", "../js/pages/Dashbord.js" , "../js/index.js"],
   theme: {
     extend: {},
   },
